0175a555c9d78caa2fe8c671dad0aaf255ea5b35
3418877951@qq.com <shine3920@163.com>
2019-11-13 17:46:31 +0800

    workbook

--- workbook/php/mail/mailFrom.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $to = "3418877961@qq.com";
-    $subject = "test mail";
-    $msg = "hello! what are you doing?";
-    $from = "3418877951@qq.com";
-    $headers = "from:$from";
-    mail($to,$subject,$msg,$from,$headers);
-    echo "mail send";
-?>
